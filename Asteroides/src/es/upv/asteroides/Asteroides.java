07073af9f72fca282a56007a02bc5a1dccba8bdb
megasonic5 <168662773+megasonic5@users.noreply.github.com>
2025-05-21 19:23:45 +0200

    Create Asteroides.java

---
+++ Asteroides/src/es/upv/asteroides/Asteroides.java
@@ -0,0 +1,12 @@
+package es.upv.asteroides;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Asteroides extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
