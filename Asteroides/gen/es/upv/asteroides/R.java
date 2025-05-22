f430028c59c9f520a1c9e0b514217b50a2b0a0b6
megasonic5 <168662773+megasonic5@users.noreply.github.com>
2025-05-22 08:19:05 +0200

    Create R.java

---
+++ Asteroides/gen/es/upv/asteroides/R.java
@@ -0,0 +1,9 @@
+package es.upv.asteroides;
+
+public final class R {
+    public static final class attr {
+    }
+    public static final class layout {
+        public static final int main = 0x00;
+    }
+}
